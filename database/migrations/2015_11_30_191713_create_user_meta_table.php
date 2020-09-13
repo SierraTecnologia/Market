24e0cbe773398503c9e8f24ecf0d4005c888c794
Ricardo R Sierra <ricardo@sierratecnologia.com.br>
2020-09-13 19:25:27 -0300

    fix laravel8 compatibility

--- database/migrations/2015_11_30_191713_create_user_meta_table.php
+++ database/migrations/2015_11_30_191713_create_user_meta_table.php
@@ -12,22 +12,44 @@ class CreateUserMetaTable extends Migration
      */
     public function up()
     {
-        Schema::create(config('app.db-prefix', '').'user_meta', function (Blueprint $table) {
-            $table->increments('id');
+        try {
+            Schema::create(config('app.db-prefix', '').'user_meta', function (Blueprint $table) {
+                $table->increments('id');
 
-            $table->integer('user_id')->unsigned();
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+                $table->integer('user_id')->unsigned();
+                $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
 
-            $table->string('phone')->nullable();
+                $table->string('phone')->nullable();
 
-            $table->boolean('is_active')->default(false);
-            $table->string('activation_token')->nullable();
+                $table->boolean('is_active')->default(false);
+                $table->string('activation_token')->nullable();
 
-            $table->boolean('marketing')->default(0);
-            $table->boolean('terms_and_cond')->default(1);
+                $table->boolean('marketing')->default(0);
+                $table->boolean('terms_and_cond')->default(1);
 
-            $table->timestamps();
-        });
+                $table->timestamps();
+            });
+            //code...
+        } catch (\Throwable $th) {
+            try {
+                Schema::create(config('app.db-prefix', '').'user_meta', function (Blueprint $table) {
+                    $table->increments('id');
+
+                    $table->integer('user_id')->unsigned();
+
+                    $table->string('phone')->nullable();
+
+                    $table->boolean('is_active')->default(false);
+                    $table->string('activation_token')->nullable();
+
+                    $table->boolean('marketing')->default(0);
+                    $table->boolean('terms_and_cond')->default(1);
+
+                    $table->timestamps();
+                });
+            } catch (\Throwable $th) {
+            }
+        }
     }
 
     /**
